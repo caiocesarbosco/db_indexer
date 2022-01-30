e0369f5aee980244c6cf46d239000b8cd2a1d1b7
caiocesarbosco <caio.c.bosco@gmail.com>
2022-01-30 14:28:09 -0300

    Use Enum for Data Representation

--- src/data.rs
+++ src/data.rs
@@ -1,26 +1,29 @@
 #[derive(PartialEq, Debug)]
-pub struct Data <'a> {
-    string_col: &'a str,
-    number_col: i64
+pub enum Data <'a>{
+    StringCol(&'a str),
+    NumberCol(i64)
 }
 
 impl<'a> Data<'a> {
 
-    pub fn new(string_val: &'a str, number_val: i64) -> Data {
-        Data {
-            string_col: string_val,
-            number_col: number_val
+    pub fn new_from_str(string_val: &'a str) -> Data {
+        Data::StringCol(string_val)
+    }
+
+    pub fn new_from_number(number_val: i64) -> Data<'a> {
+        Data::NumberCol(number_val)
+    }
+}
+
+impl<'a> From<Data<'a>> for Vec<u8> {
+    fn from(data: Data<'a>) -> Vec<u8> {
+        let vec_data: Vec<u8>;
+        match data {
+            Data::StringCol(str_data) => vec_data = str_data.as_bytes().to_vec(),
+            Data::NumberCol(number_data) => vec_data = number_data.to_be_bytes().to_vec(),
         }
+        vec_data           
     }
-
-    pub fn get_string_val(&self) -> &'a str {
-        self.string_col
-    }
-
-    pub fn get_number_val(&self) -> i64 {
-        self.number_col
-    }
-
 }
 
 #[cfg(test)]
@@ -29,27 +32,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_data_creation() {
+    fn test_string_data_creation() {
         let string_val: &str = "My String Test";
-        let number_val: i64 = 999;
-        let data = Data::new(string_val, number_val);
-        assert_eq!(data, Data {string_col: string_val, number_col: number_val});
+        let data = Data::new_from_str(string_val);
+        assert_eq!(data, Data::StringCol(string_val));
     }
 
     #[test]
-    fn test_string_col_getter() {
-        let string_val: &str = "My String Test";
+    fn test_number_data_creation() {
         let number_val: i64 = 999;
-        let data = Data::new(string_val, number_val);
-        assert_eq!(data.get_string_val(), string_val);
-    }    
+        let data = Data::new_from_number(number_val);
+        assert_eq!(data, Data::NumberCol(number_val));
+    }
 
     #[test]
-    fn test_number_col_getter() {
+    fn test_convertion_for_byte_array() {
         let string_val: &str = "My String Test";
         let number_val: i64 = 999;
-        let data = Data::new(string_val, number_val);
-        assert_eq!(data.get_number_val(), number_val);
+        let number_data = Data::new_from_number(number_val);
+        let str_data = Data::new_from_str(string_val);
+        let str_data_as_vec: Vec<u8> = str_data.into();
+        let number_data_as_vec: Vec<u8> = number_data.into();
+        assert_eq!(str_data_as_vec, Vec::from(string_val));
+        assert_eq!(number_data_as_vec, Vec::from(number_val.to_be_bytes()));
     }
 
 }
